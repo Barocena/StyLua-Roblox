b80451e35a4801ded44d7968c6423c67dcf3fa6b
Barış <34089907+Barocena@users.noreply.github.com>
2023-11-16 16:07:21 +0300

    New Serverside

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const express = require("express");
-const fs = require("fs");
+const StyLua = require("@johnnymorganz/stylua");
 const app = express();
 
-// Middleware to parse request body as text
+// StyLua Roblox Plugin
+// Version 1.0.0
+
+const EnumList = {
+  ["call_parentheses"]: StyLua.CallParenType,
+  ["collapse_simple_statement"]: StyLua.CollapseSimpleStatement,
+  ["indent_type"]: StyLua.IndentType,
+  ["line_endings"]: StyLua.LineEndings,
+  ["quote_style"]: StyLua.QuoteStyle,
+};
+
 app.use(express.text());
-
-const { exec } = require("child_process");
-
 app.post("/stylua", (req, res) => {
-  const text = req.body;
-  fs.writeFile("output.luau", text, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error writing to file");
-    } else {
-      console.log("Text written to file");
-      exec("stylua output.luau", (err, _, stderr) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send(stderr);
-        } else {
-          console.log("Stylua command executed");
-          res.setHeader("Content-Type", "text/plain");
-          res.sendFile(__dirname + "/output.luau");
-        }
-      });
+  const input = req.body;
+  const Query = req.query.Config;
+
+  var Configs = StyLua.Config.new();
+
+  if (Query) {
+    const Parsed = JSON.parse(Query);
+    for (const [key, value] of Object.entries(Parsed)) {
+      if (EnumList[key]) {
+        Configs[key] = EnumList[key][value];
+      } else if (key == "sort_requires") {
+        Configs[key] = StyLua.SortRequiresConfig.new().set_enabled(value);
+      } else {
+        Configs[key] = value;
+      }
     }
-  });
+  }
+
+  const output = StyLua.formatCode(
+    input,
+    Configs,
+    StyLua.Range.from_values(),
+    StyLua.OutputVerification.None
+  );
+
+  res.setHeader("Content-Type", "text/plain");
+  res.send(output);
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(18259, () => {
+  console.log("Server started on port 18259");
 });
